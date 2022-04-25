ee5be02c63004618331d80e9dfbc24fed3ed1dae
Andrej Macko <andrej.macko@siemens-healthineers.com>
2022-04-25 20:45:48 +0200

    get all info about restaurant

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import data_loader
+import enum_days
 import restaurant_finder
 import datetime
 
 
 def print_restaurants_name(restaurants):
-    print("Restaurants open now!")
     for i, restaurant in enumerate(restaurants):
         if i % 5 == 0 and i != 0:
             print(restaurant)
@@ -14,8 +14,21 @@ def print_restaurants_name(restaurants):
             print(f"{restaurant}, ", end=" ")
 
 
-def print_all_information(restaurants):
-    pass
+def print_work_hours(r_key, r_value):
+    print(r_key)
+    format = "%H:%M"
+    for day, hour in r_value.items():
+        print(f"{enum_days.Days(day).name}: {hour.strftime(format)}", end=' ')
+    print()
+
+
+def print_all_information(name, restaurant_info):
+    print(f"\n{name}")
+    for r_key, r_value in restaurant_info.items():
+        if r_key == 'opens' or r_key == 'closes':
+            print_work_hours(r_key, r_value)
+        else:
+            print(f"{r_key}: {r_value}")
 
 
 if __name__ == "__main__":
@@ -28,6 +41,7 @@ if __name__ == "__main__":
 
     print("Welcome to the Restaurant Finder. Here You can find the working "
           "hours of restaurant that You are interested in or some another information")
+    print("Restaurants open now!")
     print_restaurants_name(restaurants_open_now)
     while active:
         print("\nPress 1 to find all opened restaurants with specific time and day")
@@ -47,9 +61,12 @@ if __name__ == "__main__":
             print("Mon = 0, Tue = 1, Wed = 2, Thu = 3, Fri = 4, Sat = 5, Sun = 6")
             day = int(input("Day: "))
             restaurants = rf.opened_restaurants(hours, day)
+            print(f"Restaurants open on {enum_days.Days(day).name} at {hours}")
             print_restaurants_name(restaurants)
         elif user_input == 2:
-            pass
+            restaurant_name = input("Enter restaurant name: ")
+            info = rf.get_information_about_specific_restaurant(restaurant_name)
+            print_all_information(restaurant_name, info)
         elif user_input == 3:
             pass
         elif user_input == 4:
